28d54f25a6e5d5f1e8018a055d897c9acceb72e5
Lukáš Růžička <lukas.ruzicka@synetech.cz>
2018-03-26 11:21:42 +0200

    UI set up

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Lukáš Růžička on 26.03.18.
+//  Copyright © 2018 Lukáš Růžička. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var detailsLbl: UILabel!
+    
+    
+
+}
