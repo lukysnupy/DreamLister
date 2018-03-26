b3b3884609f5bd59df2df39389a653ca188508a6
Lukáš Růžička <lukas.ruzicka@synetech.cz>
2018-03-26 14:31:35 +0200

    Showing details view, items can be added and updated

---
+++ DreamLister/Controller/ItemDetailsVC.swift
@@ -0,0 +1,132 @@
+//
+//  ItemDetailsVC.swift
+//  DreamLister
+//
+//  Created by Lukáš Růžička on 26.03.18.
+//  Copyright © 2018 Lukáš Růžička. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
+    var itemToEdit: Item?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+       
+//        let store = Store(context: context)
+//        store.name = "Amazon"
+//        let store2 = Store(context: context)
+//        store2.name = "Ebay"
+//        let store3 = Store(context: context)
+//        store3.name = "Alza"
+//        let store4 = Store(context: context)
+//        store4.name = "AliExpress"
+//        let store5 = Store(context: context)
+//        store5.name = "Tesla Dealership"
+//        let store6 = Store(context: context)
+//        store6.name = "iSTYLE"
+//
+//        ad.saveContext()
+        
+        getStores()
+        
+        if itemToEdit != nil {
+            loadItemData()
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return stores[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch let error as NSError {
+            print("\(error)")
+        }
+    }
+    
+    @IBAction func saveBtnClicked(_ sender: Any) {
+        var item: Item!
+        
+        if itemToEdit == nil {
+            item = Item(context: context)
+        } else {
+            item = itemToEdit
+        }
+        
+        if let title = titleField.text {
+            item.title = title
+        }
+        
+        if let price = priceField.text {
+            item.price = (price as NSString).doubleValue
+        }
+        
+        if let details = detailsField.text {
+            item.details = details
+        }
+        
+        item.toStore = stores[storePicker.selectedRow(inComponent: 0)]
+        
+        ad.saveContext()
+        
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func removeBtnClicked(_ sender: Any) {
+        
+    }
+    
+    func loadItemData() {
+        if let item = itemToEdit {
+            titleField.text = item.title
+            priceField.text = "\(item.price)"
+            detailsField.text = item.details
+            
+            if let store = item.toStore {
+                var index = 0
+                repeat {
+                    if stores[index].name == store.name {
+                        storePicker.selectRow(index, inComponent: 0, animated: false)
+                        break
+                    }
+                    index += 1
+                } while (index < stores.count)
+            }
+        }
+    }
+    
+}
--- DreamLister/Controller/MainVC.swift
+++ DreamLister/Controller/MainVC.swift
@@ -37,6 +37,23 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         let item = controller.object(at: indexPath as IndexPath)
         cell.configureCell(item: item)
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let objs = controller.fetchedObjects , objs.count > 0 {
+            let item = objs[indexPath.row]
+            performSegue(withIdentifier: "ItemDetailsVC", sender: item)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ItemDetailsVC" {
+            if let destination = segue.destination as? ItemDetailsVC {
+                if let item = sender as? Item {
+                    destination.itemToEdit = item
+                }
+            }
+        }
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let sections = controller.sections {
@@ -63,6 +80,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         fetchRequest.sortDescriptors = [dateSort]
         
         self.controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        controller.delegate = self
         
         do {
             try self.controller.performFetch()
